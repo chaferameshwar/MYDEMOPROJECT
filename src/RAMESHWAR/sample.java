d98fbc289f1ad9340d11a2527158f206e27148ba
ankit <ankit@LAPTOP-48PEFS3E>
2021-05-02 13:57:21 +0530

    [Ankita]: changes on project

--- src/RAMESHWAR/sample.java
+++ src/RAMESHWAR/sample.java
@@ -3,6 +3,7 @@ package RAMESHWAR;
 public class sample {
 
 	public static void main(String[] args) {
-		System.out.println("ankita bai");
+		System.out.println("ankita bai replied to Ramu Kaka");
+		System.out.println("How dare you to say like that");
 	}
 }
